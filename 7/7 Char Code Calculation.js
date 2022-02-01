8489fe816eba7c33f189ea6cf8d0d610e6e3928d
hsargeant <hssoftwareengineering@gmail.com>
2022-02-01 16:15:18 +0100

    daily coding challenge

---
+++ 7/7 Char Code Calculation.js
@@ -0,0 +1,31 @@
+// DESCRIPTION:
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits in total1 and total2:
+
+//   (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+//                        6
+
+function calc(x){
+  console.log(x)
+ let s =  x.split("").reduce((a,_,i)=>{
+    return a+x.charCodeAt(i)
+  },"")
+ 
+ let s1 = s.split("").map(x=>{
+   if(x!=7){
+     return x
+   }else return "1"
+ })
+ 
+return ( s.split("").reduce((a,x)=>+a + +x,0))  - (s1.reduce((a,x)=>+a + +x,0))
+}
