40313cbfccd010dd03bf199253320e8ed2f48a94
Hsargeant <hssoftwareengineering@gmail.com>
2022-03-29 15:20:19 +0100

    daily coding challenges

---
+++ 8-Kyu/8 kyu Array plus array.js
@@ -0,0 +1,11 @@
+// Sum of two arrays
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+ 
+ return arr1.reduce((a,b)=>a+b,0)+arr2.reduce((a,b)=>a+b,0)
+  
+  
+  
+}
