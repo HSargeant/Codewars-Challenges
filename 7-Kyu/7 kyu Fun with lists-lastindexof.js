2aa8ef712d4d6a118373744ab49fbd88728a2090
Hsargeant <hssoftwareengineering@gmail.com>
2022-05-12 17:20:19 +0100

    fun with list series

---
+++ 7-Kyu/7 kyu Fun with lists-lastindexof.js
@@ -0,0 +1,20 @@
+// Implement the method lastIndexOf (last_index_of in PHP and Python), which accepts a linked list (head) and a value, and returns the index (zero based) of the last occurrence of that value if exists, or -1 otherwise.
+
+// For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, lastIndexOf / last_index_of should return 3.
+
+// The linked list is defined as follows:
+
+// function Node(data, next = null) {
+//   this.data = data;
+//   this.next = next;
+// }
+// Note: the list may be null/None and can hold any type of value.
+
+function lastIndexOf(head, value) {
+  let arr = [];
+  while (head) {
+    arr.push(head.data);
+    head = head.next;
+  }
+  return arr.lastIndexOf(value);
+}
