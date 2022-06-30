21ec6a73bf00a869b1e69a0714f7f8adf5abb716
hsargeant <hssoftwareengineering@gmail.com>
2022-06-30 16:15:18 +0100

    daily coding challenge

---
+++ 6/6 Extract last names of people named Michael.js
@@ -0,0 +1,29 @@
+// DESCRIPTION:
+// Given a text, for example:
+
+// const inputText = "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+
+// get an array of last names of people named Michael. The result should be:
+// ["Jordan", "Johnson", "Green", "Wood"]
+
+// Notes:
+
+// First name will always be Michael with upper case 'M'.
+// There will always be a space character between 'Michael' and last name.
+// Last name will always be one word, starting with an upper-case letter and continuing with lower-case letters.
+// There will always be at least one 'Micheal' with a valid last name in the input text.
+
+function getMichaelLastName(str) {
+  let arr=[]
+  let x = str.split(/\W/)
+
+  while(x.includes("Michael")){
+    let index = x.indexOf("Michael")
+    if(x[index+1] && x[index+1][0].match(/[A-Z]/)){
+      arr.push(x[index+1])
+    }
+    x=x.slice(index+1)
+    
+  }
+return arr
+}
