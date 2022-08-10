29e1beddac1ca459e3a2901b09a199908ec3f4f3
hsargeant <hssoftwareengineering@gmail.com>
2022-08-10 17:33:25 +0100

    daily coding challenge

---
+++ 7/function factorial(n).js
@@ -0,0 +1,11 @@
+function factorial(n)
+{if(n==0) return 1
+ if(n<0 || n>12) throw RangeError
+ let res=1
+  for(i=1; i<=n;i++){
+    res*=i
+  }
+ return res
+}
+
+// write a function to calculate facotorial
