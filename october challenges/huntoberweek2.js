f5175c4fbcf1f24ddfc29a7595340fef47c263f3
hsargeant <hssoftwareengineering@gmail.com>
2022-10-11 21:41:16 -0400

    daily coding challenge

--- october challenges/huntoberweek2.js
+++ october challenges/huntoberweek2.js
@@ -55,3 +55,94 @@ const moveLeft=(a,e)=>{
     a[index-1] = temp
     return a
 }
+
+// day3
+// Today I'd like you to write a function that:
+
+// takes in a given array of strings and move any entries containing the letter 'a' to the front
+// then move any remaining entries that have over 3 characters to the back
+// please preserve the relative order of entries within their given category
+// Example:
+
+
+// COPY
+// myArr = ['hi', 'hello', 'howdy', 'hola', 'hej', 'hallo', 'heyyy']
+// // move things around
+// => ['hola', 'hallo', 'hi', 'hej', 'hello', 'howdy', 'heyyy']
+
+function moveItems(a){
+  let front = a.filter(x=>x.includes("a"))
+  let back = a.filter(x=>x.length>3 &&!x.includes("a"))
+  a=a.filter(x=>!x.includes("a")&&x.length<3)
+  return [...front,...a,...back]
+}
+
+// day4
+
+// Imagine, if you will, an array with length X, with each of its entries having X number of primitives as their entries. Now imagine it arranged as a grid.
+
+
+// COPY
+// // for the less imaginative among us :) 
+// [[0,1,2],[3,4,5],[6,7,8]]
+
+// // as a grid
+// [
+// [0,1,2],
+// [3,4,5],
+// [6,7,8]
+// ]
+// Still with me? I hope so! Today I need your help moving some things around again. I'm always rearranging.
+
+// Please write twin functions that each take in a given value that will only appear once within a given array of a similar structure to the one described above, which will either move that value up one row or down one row in the array, keeping its same horizontal position. It will essentially swap places with whatever was where it needed to be. Just like in Day 2, don't do anything if the given value is already as high or low as it can get.
+
+// Oh, and go ahead and mutate the given array. We're livin' on the wild side!
+
+// Examples:
+
+// COPY
+// myGrid = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
+
+// // call move up function with 'h' and myGrid
+// console.log(myGrid) = [['a', 'b', 'c'], ['d', 'h', 'f'], ['g', 'e', 'i']]
+
+// // call move down function this time, with 'f' and myGrid as arguments
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
+
+// // call move down function again, same arguments
+// // Note that 'f' is already as far down as it can go
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
+
+function moveUp(a,t){
+  let j
+  for(i=0; i<a.length;i++){
+      if(a[i].includes(t)){
+          j=a[i].indexOf(t)
+          break
+      }
+  }  
+  if(a[i-1]){
+      let temp = a[i][j]
+      a[i][j] = a[i-1][j]
+      a[i-1][j]=temp
+  }
+  return a
+  }
+  
+  function moveDown(a,t){
+  let j
+  for(i=0; i<a.length;i++){
+      if(a[i].includes(t)){
+          j=a[i].indexOf(t)
+          break
+      }
+  }
+  let index = [i,j]
+  
+  if(a[i+1]){
+      let temp = a[i][j]
+      a[i][j] = a[i+1][j]
+      a[i+1][j]=temp
+  }
+  return a
+  }
