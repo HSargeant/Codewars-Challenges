ab1adea78d976cbefc37bac3998b3d7bb7322af1
hsargeant <hssoftwareengineering@gmail.com>
2022-12-11 10:17:13 -0500

    daily coding challenge

---
+++ advent-of-code-2022/9-rope.js
@@ -0,0 +1,208 @@
+const input= 'L 2\nD 2\nR 2\nU 1\nL 1\nU 1\nR 1\nU 2\nR 2\nU 2\nL 2\nR 2\nU 2\nL 2\nU 1\nR 1\nU 1\nR 2\nU 1\nD 2\nU 2\nL 2\nR 1\nU 2\nR 2\nU 2\nL 1\nD 1\nL 1\nR 2\nL 2\nD 1\nL 1\nR 2\nU 2\nL 1\nU 1\nL 2\nR 1\nL 1\nD 2\nU 2\nL 2\nD 1\nR 2\nD 1\nU 2\nD 2\nL 2\nD 1\nL 1\nR 1\nD 2\nU 2\nL 2\nU 1\nL 1\nU 2\nR 2\nD 1\nU 2\nR 2\nU 1\nL 2\nR 2\nD 2\nU 2\nD 2\nL 2\nD 1\nU 2\nL 1\nR 1\nL 1\nD 2\nU 1\nD 1\nL 2\nD 1\nL 1\nR 1\nU 1\nL 2\nR 2\nU 2\nD 2\nL 1\nR 2\nD 1\nR 2\nD 2\nL 1\nU 2\nR 1\nD 2\nL 1\nD 2\nU 2\nD 1\nU 2\nR 1\nD 1\nL 1\nD 2\nR 2\nU 1\nR 1\nD 2\nL 2\nR 2\nD 1\nR 3\nU 1\nD 1\nU 1\nR 2\nU 2\nR 3\nU 3\nL 3\nD 2\nL 3\nD 1\nL 3\nD 3\nR 3\nD 3\nR 3\nL 2\nD 3\nR 1\nL 3\nU 1\nL 1\nU 1\nL 2\nR 3\nD 2\nU 3\nR 3\nD 2\nL 3\nR 3\nD 1\nL 3\nU 1\nD 1\nL 1\nD 2\nL 2\nU 1\nL 1\nR 1\nU 3\nR 2\nL 3\nU 2\nR 2\nU 1\nD 3\nU 1\nL 1\nU 2\nR 1\nD 3\nR 3\nU 3\nD 2\nU 1\nL 1\nD 3\nL 2\nU 3\nD 1\nR 1\nD 1\nR 3\nU 2\nL 1\nU 3\nR 1\nU 2\nL 3\nR 2\nL 1\nR 1\nU 2\nL 1\nD 2\nL 3\nD 3\nR 3\nD 1\nU 3\nD 1\nR 3\nU 2\nL 2\nD 1\nU 1\nR 3\nL 2\nD 2\nL 2\nD 2\nR 1\nD 3\nL 1\nR 2\nL 2\nR 3\nL 1\nU 1\nD 2\nR 2\nU 3\nR 1\nL 2\nR 3\nU 1\nL 3\nD 3\nL 1\nD 1\nU 4\nR 2\nL 1\nD 3\nU 1\nD 3\nR 2\nL 2\nD 2\nL 3\nU 4\nD 4\nL 4\nU 1\nD 2\nL 3\nR 1\nL 1\nU 2\nD 1\nU 3\nL 2\nR 2\nD 2\nL 2\nR 4\nD 1\nL 2\nR 4\nU 4\nL 3\nR 3\nD 1\nL 3\nU 3\nL 1\nU 3\nL 1\nD 4\nU 1\nR 3\nL 2\nR 3\nD 2\nL 4\nU 1\nD 4\nL 2\nU 2\nD 3\nL 2\nU 3\nR 1\nU 1\nR 2\nU 3\nL 1\nR 4\nD 2\nR 1\nD 1\nL 4\nR 3\nU 1\nD 2\nL 3\nR 4\nL 1\nU 1\nD 2\nU 1\nD 3\nR 3\nL 3\nU 1\nL 3\nR 3\nD 2\nU 2\nL 2\nR 1\nL 3\nU 3\nR 4\nD 1\nU 4\nD 1\nR 2\nD 4\nL 2\nR 1\nL 2\nR 1\nD 3\nU 3\nR 1\nU 1\nR 1\nD 2\nR 4\nD 4\nL 3\nD 4\nR 1\nD 1\nU 1\nL 1\nR 1\nD 4\nL 1\nR 1\nU 4\nR 5\nD 4\nR 5\nU 1\nD 1\nR 1\nU 4\nL 3\nD 5\nU 3\nL 2\nD 3\nR 5\nU 1\nD 1\nR 2\nL 1\nR 3\nD 4\nR 5\nL 1\nU 3\nD 3\nR 1\nD 4\nR 1\nL 4\nR 3\nU 1\nD 1\nU 5\nR 2\nL 3\nU 1\nL 4\nD 1\nR 3\nL 4\nD 2\nR 3\nL 5\nD 2\nL 3\nD 2\nL 2\nR 4\nD 1\nR 3\nL 5\nU 2\nL 4\nD 3\nL 2\nR 4\nD 5\nL 5\nR 4\nU 5\nD 2\nU 4\nD 4\nL 3\nU 3\nL 3\nD 1\nR 4\nD 4\nU 3\nR 4\nL 3\nD 2\nL 4\nD 2\nL 4\nR 3\nL 5\nR 2\nL 3\nR 3\nU 4\nD 4\nL 4\nR 2\nD 4\nU 3\nL 3\nU 1\nD 2\nU 5\nL 2\nD 3\nU 1\nR 5\nD 2\nL 3\nU 1\nD 2\nL 2\nU 2\nR 5\nL 4\nR 4\nL 2\nD 3\nR 1\nU 5\nR 2\nL 5\nU 3\nL 3\nR 6\nD 2\nL 2\nR 2\nU 1\nD 6\nU 6\nD 1\nL 3\nR 6\nU 6\nR 3\nD 6\nR 5\nU 5\nD 1\nR 4\nL 2\nU 2\nR 4\nD 6\nU 4\nR 4\nD 6\nR 2\nU 1\nD 5\nU 3\nR 1\nL 2\nR 1\nU 6\nR 1\nL 1\nU 4\nR 1\nD 6\nU 2\nD 5\nR 2\nD 6\nR 3\nL 3\nU 1\nR 3\nL 4\nD 6\nL 3\nU 3\nD 1\nU 4\nR 6\nL 3\nD 2\nU 6\nR 2\nD 4\nR 3\nD 5\nU 2\nL 1\nD 4\nR 1\nL 3\nR 5\nU 4\nD 1\nL 3\nU 4\nR 2\nD 6\nR 4\nD 5\nR 5\nD 3\nR 6\nL 4\nU 2\nL 4\nU 2\nL 5\nD 2\nR 3\nD 2\nR 6\nU 4\nR 5\nU 1\nR 6\nD 4\nL 2\nD 2\nU 2\nL 5\nR 1\nU 2\nR 3\nU 5\nL 3\nD 1\nL 1\nR 1\nL 3\nU 1\nL 4\nR 3\nL 6\nU 4\nL 2\nD 2\nU 7\nR 7\nU 4\nR 5\nD 4\nL 7\nU 5\nL 5\nU 1\nD 5\nL 3\nR 4\nD 3\nR 5\nL 3\nD 3\nL 4\nU 5\nL 4\nR 2\nL 5\nU 4\nL 4\nD 4\nU 5\nL 3\nU 7\nR 2\nL 1\nU 2\nL 7\nU 5\nR 4\nD 5\nR 1\nL 3\nD 6\nR 7\nL 1\nR 5\nD 4\nU 4\nL 7\nD 6\nU 7\nR 4\nU 2\nR 2\nU 2\nR 7\nD 6\nR 4\nL 1\nR 4\nD 3\nU 5\nR 3\nL 5\nR 2\nL 3\nD 2\nR 1\nD 3\nL 7\nD 4\nL 7\nU 7\nD 5\nR 6\nU 7\nR 5\nD 6\nR 5\nL 4\nU 1\nD 2\nL 4\nD 5\nL 5\nD 2\nL 7\nR 2\nU 7\nL 6\nD 3\nU 3\nD 6\nU 1\nR 3\nL 1\nU 7\nR 2\nD 2\nU 3\nL 6\nD 5\nU 5\nD 2\nR 6\nD 3\nL 3\nR 2\nL 6\nD 1\nL 2\nD 1\nL 7\nU 1\nD 2\nU 3\nL 5\nD 7\nR 2\nL 8\nU 7\nL 4\nU 4\nD 1\nU 4\nD 3\nR 8\nU 3\nL 4\nR 1\nL 5\nR 1\nL 1\nD 8\nR 5\nL 2\nU 1\nL 8\nU 8\nD 1\nR 3\nU 1\nR 1\nU 5\nR 4\nL 4\nU 7\nD 1\nU 8\nL 4\nD 7\nU 8\nL 5\nD 2\nR 4\nL 3\nR 2\nL 2\nD 1\nU 4\nD 2\nL 8\nU 1\nR 6\nD 5\nR 7\nD 2\nU 4\nL 4\nR 3\nU 2\nD 8\nR 3\nL 3\nU 2\nR 1\nD 5\nU 2\nR 5\nD 8\nR 7\nU 2\nL 6\nU 2\nD 1\nL 4\nU 1\nD 7\nU 4\nL 3\nU 5\nR 5\nD 4\nR 7\nU 4\nL 6\nR 4\nU 5\nD 7\nU 5\nD 3\nL 5\nU 1\nL 8\nD 8\nR 3\nL 6\nR 6\nU 1\nR 8\nL 8\nU 6\nR 2\nL 6\nU 2\nL 4\nD 1\nL 1\nU 5\nD 3\nR 6\nD 6\nL 7\nD 7\nL 1\nR 7\nU 4\nL 7\nU 6\nR 1\nL 8\nD 6\nL 5\nU 2\nD 7\nR 8\nD 1\nL 6\nR 1\nL 8\nD 3\nR 8\nL 8\nU 7\nR 3\nU 6\nL 2\nR 9\nL 3\nR 9\nD 1\nL 2\nR 7\nL 8\nU 1\nD 9\nU 7\nL 9\nR 1\nL 8\nD 7\nR 4\nL 9\nU 1\nL 2\nD 7\nR 7\nD 5\nL 4\nD 7\nU 9\nR 9\nD 4\nR 6\nD 5\nL 1\nR 8\nU 8\nL 6\nR 4\nL 3\nU 4\nD 7\nR 8\nD 8\nR 2\nL 4\nR 4\nU 1\nL 8\nU 5\nL 9\nU 4\nR 9\nL 1\nR 1\nD 5\nL 5\nD 8\nU 2\nL 4\nU 5\nD 4\nL 1\nD 1\nR 1\nU 9\nR 4\nD 2\nU 6\nL 5\nR 7\nD 9\nL 3\nU 2\nL 8\nU 6\nD 4\nR 1\nU 4\nR 1\nU 6\nD 8\nU 7\nR 5\nD 4\nR 2\nD 6\nU 6\nR 1\nU 3\nD 8\nU 2\nR 1\nU 8\nD 9\nU 3\nR 3\nD 6\nR 8\nD 1\nL 2\nD 2\nR 3\nD 3\nU 3\nD 2\nR 2\nL 5\nR 1\nU 9\nL 9\nD 3\nR 2\nL 10\nR 6\nL 5\nR 4\nL 6\nR 8\nD 2\nL 1\nR 2\nD 4\nL 9\nD 6\nU 2\nR 7\nU 9\nD 5\nR 2\nL 10\nD 7\nL 8\nU 3\nR 4\nU 4\nL 7\nU 10\nR 8\nL 7\nU 10\nR 9\nD 4\nR 2\nL 10\nR 9\nL 4\nD 7\nL 10\nD 5\nU 4\nR 3\nL 7\nD 3\nL 3\nR 3\nD 10\nR 2\nL 6\nD 5\nR 10\nL 7\nU 1\nD 6\nL 3\nD 4\nL 3\nD 7\nR 10\nU 6\nR 2\nU 5\nR 7\nU 3\nR 2\nU 6\nR 5\nL 9\nR 5\nD 3\nU 4\nL 5\nR 6\nU 1\nL 6\nU 7\nD 10\nR 10\nU 9\nL 10\nD 10\nR 6\nL 1\nU 8\nR 10\nU 8\nR 10\nU 1\nD 8\nU 10\nR 9\nD 8\nR 4\nD 10\nL 4\nU 9\nL 1\nD 3\nR 1\nL 3\nD 1\nL 9\nR 5\nD 7\nR 3\nL 4\nR 8\nU 6\nD 7\nU 3\nR 1\nD 8\nL 3\nR 9\nU 8\nR 11\nD 5\nL 5\nU 3\nR 6\nU 10\nR 8\nD 1\nU 3\nD 1\nR 11\nD 8\nU 11\nR 7\nU 6\nR 7\nU 6\nL 7\nD 4\nU 10\nL 4\nU 1\nL 10\nD 3\nR 3\nU 9\nL 11\nU 10\nR 6\nU 10\nR 10\nD 9\nL 10\nR 6\nL 2\nR 6\nL 1\nD 10\nU 8\nD 11\nU 2\nL 9\nU 7\nR 8\nL 10\nR 11\nD 4\nL 9\nR 3\nU 1\nL 8\nD 1\nL 9\nU 8\nD 8\nU 5\nR 6\nD 5\nR 9\nD 1\nL 11\nR 4\nD 11\nR 9\nL 11\nR 1\nU 5\nL 3\nD 4\nR 6\nD 9\nR 4\nL 7\nU 3\nD 6\nU 8\nD 1\nU 8\nD 5\nU 11\nR 7\nU 1\nL 1\nU 5\nL 4\nD 1\nR 1\nL 8\nU 9\nD 10\nU 4\nL 1\nR 4\nD 11\nR 8\nD 8\nR 11\nD 12\nU 3\nL 12\nU 10\nL 9\nR 11\nD 10\nR 5\nD 12\nU 6\nL 12\nR 3\nD 7\nU 11\nL 6\nU 5\nR 8\nD 3\nU 8\nD 8\nR 10\nD 12\nL 4\nR 10\nL 10\nU 1\nL 7\nR 10\nL 7\nU 4\nR 1\nL 10\nR 8\nD 11\nL 2\nU 2\nL 6\nU 9\nR 10\nL 2\nR 10\nU 4\nR 9\nD 7\nL 1\nU 7\nL 12\nD 1\nL 9\nU 7\nL 8\nR 12\nL 3\nD 9\nU 7\nL 11\nD 6\nR 4\nD 2\nR 11\nU 10\nR 8\nD 12\nU 10\nR 4\nL 11\nR 2\nU 11\nL 7\nR 6\nL 12\nR 5\nU 7\nL 6\nR 12\nD 6\nR 2\nL 10\nU 4\nD 5\nU 11\nR 3\nD 10\nL 2\nU 9\nL 3\nD 6\nU 10\nL 3\nU 10\nL 7\nR 8\nU 10\nD 3\nR 3\nL 12\nD 12\nL 9\nR 2\nD 2\nR 9\nL 4\nD 5\nL 2\nR 12\nD 2\nR 4\nU 2\nD 4\nU 3\nR 13\nL 3\nU 9\nR 5\nU 2\nD 8\nL 7\nD 10\nL 1\nU 2\nR 13\nD 11\nL 7\nD 6\nL 11\nR 12\nU 2\nD 9\nR 11\nU 9\nD 9\nL 2\nD 13\nU 3\nL 7\nD 3\nL 10\nR 5\nU 4\nL 5\nU 8\nL 4\nU 11\nR 3\nL 3\nD 1\nL 12\nD 8\nR 12\nL 1\nU 6\nD 2\nU 13\nL 5\nU 4\nR 12\nL 11\nU 1\nD 3\nU 11\nD 1\nR 2\nL 6\nD 11\nR 5\nD 7\nR 2\nU 9\nL 5\nR 4\nU 4\nR 9\nU 9\nL 9\nU 3\nR 10\nL 5\nR 5\nU 13\nD 2\nU 2\nL 3\nR 1\nU 11\nD 10\nL 7\nD 8\nR 10\nU 10\nL 13\nU 3\nL 9\nD 2\nR 13\nU 2\nD 10\nU 8\nL 5\nU 1\nD 10\nL 11\nR 5\nD 8\nL 11\nD 3\nR 13\nD 11\nL 8\nD 6\nL 3\nU 13\nL 9\nU 8\nR 8\nU 11\nD 1\nU 7\nD 2\nR 8\nL 4\nD 6\nU 10\nL 6\nR 2\nD 8\nR 5\nL 7\nU 4\nD 9\nU 4\nD 12\nL 12\nR 3\nL 12\nD 7\nL 3\nU 10\nD 3\nU 5\nR 3\nU 4\nL 4\nR 9\nL 8\nR 11\nD 5\nR 10\nD 13\nU 1\nL 4\nD 5\nR 13\nU 1\nR 8\nU 9\nD 2\nR 8\nD 4\nU 14\nR 14\nD 13\nU 2\nR 1\nU 3\nD 11\nL 3\nU 2\nD 10\nL 6\nU 9\nR 13\nD 10\nU 13\nD 4\nL 11\nD 9\nU 6\nL 6\nU 6\nD 4\nL 7\nD 14\nL 11\nD 1\nL 12\nR 2\nL 10\nU 3\nD 5\nU 12\nD 6\nU 6\nD 3\nU 8\nR 3\nD 11\nR 7\nD 4\nR 3\nU 12\nR 10\nD 9\nU 12\nR 6\nL 13\nD 1\nL 3\nU 1\nR 14\nL 8\nD 11\nL 5\nU 10\nL 12\nD 9\nR 11\nL 14\nD 8\nL 2\nR 9\nD 7\nU 4\nL 9\nD 9\nU 6\nD 5\nR 5\nL 10\nR 12\nL 13\nU 8\nL 2\nD 3\nL 8\nU 3\nR 14\nD 9\nU 8\nD 7\nU 2\nL 5\nR 11\nU 12\nL 3\nU 13\nD 4\nR 9\nU 8\nD 14\nR 14\nL 5\nD 4\nU 12\nL 1\nD 11\nR 1\nU 12\nD 1\nU 9\nL 14\nD 4\nR 15\nD 10\nU 14\nR 13\nU 11\nD 8\nL 1\nU 5\nR 2\nD 10\nU 9\nR 11\nD 2\nL 15\nR 3\nU 2\nD 4\nU 15\nD 8\nL 6\nD 1\nL 4\nU 2\nD 11\nR 6\nU 2\nR 13\nU 4\nL 5\nD 2\nR 5\nU 10\nR 4\nD 1\nR 9\nD 11\nL 13\nU 13\nR 2\nL 3\nR 7\nU 10\nR 14\nL 15\nR 1\nU 12\nD 6\nR 10\nL 12\nR 13\nD 11\nU 15\nR 8\nL 12\nD 6\nR 10\nD 4\nL 1\nR 5\nL 10\nU 2\nR 1\nL 9\nD 15\nR 7\nU 8\nD 8\nL 1\nD 14\nU 5\nD 4\nL 1\nR 6\nL 15\nU 13\nD 6\nL 5\nD 13\nR 9\nD 7\nL 15\nR 2\nD 14\nR 12\nD 13\nL 6\nU 5\nL 13\nU 16\nL 3\nD 12\nL 13\nU 11\nL 5\nU 6\nD 5\nL 15\nD 2\nR 6\nL 2\nR 5\nL 6\nR 16\nD 4\nL 10\nR 9\nL 9\nD 5\nR 6\nD 8\nU 6\nD 1\nR 4\nU 6\nD 10\nR 4\nL 12\nD 10\nR 2\nD 5\nU 15\nR 12\nU 6\nD 15\nR 5\nD 1\nL 9\nU 16\nR 3\nD 6\nL 1\nR 12\nD 12\nU 3\nR 1\nD 2\nL 11\nR 7\nD 7\nU 10\nL 8\nU 15\nL 7\nU 12\nD 10\nR 2\nL 8\nR 12\nD 16\nR 13\nL 15\nU 1\nR 1\nU 13\nL 6\nR 12\nL 5\nD 5\nU 12\nD 4\nL 7\nR 11\nL 12\nU 16\nD 11\nU 7\nL 1\nU 11\nD 7\nU 14\nD 14\nR 10\nD 4\nR 5\nD 2\nR 13\nL 13\nD 6\nL 16\nR 1\nL 4\nD 3\nU 15\nR 1\nU 2\nD 2\nU 2\nD 3\nU 7\nD 16\nU 11\nL 8\nR 1\nD 6\nU 1\nR 5\nL 6\nU 5\nD 3\nL 13\nU 8\nL 11\nD 17\nU 12\nL 16\nU 1\nD 6\nU 17\nD 7\nU 12\nR 3\nU 6\nL 10\nR 1\nL 14\nU 11\nL 8\nU 10\nL 14\nD 4\nR 3\nL 14\nD 13\nL 17\nR 12\nU 9\nD 7\nU 2\nR 10\nL 16\nU 9\nR 17\nD 14\nU 9\nL 13\nR 13\nL 5\nU 14\nL 4\nR 10\nU 2\nL 9\nD 17\nL 17\nU 9\nD 9\nL 4\nU 15\nL 2\nU 17\nR 1\nL 5\nR 2\nU 5\nR 5\nL 17\nR 4\nU 12\nR 11\nL 17\nU 11\nD 15\nU 10\nR 7\nD 9\nL 15\nU 1\nD 6\nL 13\nR 7\nD 12\nL 6\nU 14\nR 11\nD 2\nL 4\nR 14\nL 17\nR 2\nD 1\nR 13\nL 12\nD 12\nL 16\nD 14\nL 4\nR 14\nU 15\nL 17\nU 9\nD 13\nR 6\nD 2\nR 4\nL 8\nU 16\nR 10\nL 6\nU 7\nD 4\nL 15\nD 2\nU 14\nL 5\nR 8\nL 2\nD 14\nU 7\nR 5\nU 13\nL 9\nD 2\nU 18\nR 9\nU 13\nR 9\nL 5\nD 10\nU 18\nD 13\nU 13\nD 14\nL 3\nR 2\nL 13\nD 11\nR 17\nL 15\nD 3\nL 1\nU 10\nL 14\nD 15\nL 13\nR 12\nD 4\nL 1\nD 2\nR 16\nD 10\nU 11\nL 12\nU 1\nD 2\nR 3\nU 2\nR 12\nL 6\nD 1\nL 13\nR 11\nU 8\nL 8\nD 10\nR 5\nU 6\nL 7\nR 4\nU 9\nL 6\nD 10\nL 14\nU 11\nD 1\nL 7\nR 3\nU 16\nL 11\nD 13\nL 14\nU 10\nL 3\nR 12\nU 9\nR 11\nU 10\nL 8\nD 5\nU 9\nL 16\nR 17\nD 3\nR 5\nD 13\nU 8\nR 5\nU 16\nR 2\nU 13\nD 16\nL 9\nD 14\nL 15\nU 15\nL 1\nD 8\nL 7\nD 1\nR 11\nD 16\nR 9\nU 14\nL 18\nU 4\nD 14\nL 14\nD 9\nR 6\nL 1\nR 4\nL 11\nD 17\nL 9\nR 10\nL 19\nU 12\nD 15\nR 9\nL 3\nD 13\nR 6\nD 19\nR 9\nD 1\nL 13\nD 8\nL 19\nU 14\nR 18\nD 10\nR 7\nL 18\nU 18\nD 5\nL 1\nU 10\nD 10\nU 19\nR 14\nL 7\nD 19\nU 10\nL 3\nU 17\nD 1\nL 5\nU 7\nR 16\nD 3\nU 8\nD 17\nU 17\nD 17\nL 14\nD 19\nR 8\nD 14\nL 7\nU 7\nL 5\nD 6\nL 1\nU 14\nL 10\nU 15\nL 7\nD 14\nL 11\nR 5\nD 14\nL 13\nR 2\nL 19\nD 7\nR 8\nL 16\nU 4\nL 9\nU 17\nR 15\nL 8\nU 15\nD 5\nR 8\nL 16\nR 6\nD 17\nU 19\nD 19\nR 10\nD 4\nU 12\nD 2\nL 18\nD 13\nL 7\nU 17\nL 13\nD 5\nU 2\nL 6\nR 4\nU 19\nL 4\nD 8\nU 7\nL 3\nR 4\nU 17\nL 1\nU 6\nR 9\nD 16\nR 4\nD 9\nR 9\nL 11\nU 17\nD 18\nU 18\nR 16\nL 7\nR 18\nU 5\nL 4\nD 7'.split("\n").map(x=>x.split(" "))
+
+// part one
+
+function tailVisited(input){
+    let visit = new Set()
+    let hx=0,hy=0,tx=0,ty=0
+    visit.add(`0,0`)
+    for(i=0; i<input.length; i++){
+        let [dir, count ] =input[i]
+        if(dir=="U"){
+            while(count){
+                hy+=1
+                count--
+                if(tx==hx &&hy>ty+1) {
+                    ty+=1
+                    visit.add(`${tx},${ty}`)
+                }else{
+                    if(hy>ty+1&&hx==tx+1){
+                        tx+=1
+                        ty+=1
+                        visit.add(`${tx},${ty}`)
+                    }else if(hy>ty+1&&hx==tx-1){
+                        ty+=1
+                        tx-=1
+                        visit.add(`${tx},${ty}`)
+                    }
+                }
+            }
+        }else if(dir=="D"){
+            while(count){
+                hy-=1
+                count--
+                if(tx==hx &&hy<ty-1) {
+                    ty-=1
+                    visit.add(`${tx},${ty}`)
+                }else{
+                    if(hy<ty-1&&hx==tx+1){
+                        tx+=1
+                        ty-=1
+                        visit.add(`${tx},${ty}`)
+                    }else if(hy<ty-1&&hx==tx-1){
+                        ty-=1
+                        tx-=1
+                        visit.add(`${tx},${ty}`)
+                    }
+                }
+            }
+        }else if(dir=="R"){
+            while(count){
+                hx+=1
+                count--
+                if(hy==ty && hx>tx+1) {
+                    tx+=1
+                    visit.add(`${tx},${ty}`)
+                }else{
+                    if(hx>tx+1 &&hy==ty+1){
+                        ty+=1
+                        tx+=1
+                        visit.add(`${tx},${ty}`)
+                    }else if(hx>tx+1 &&hy==ty-1){
+                        ty-=1
+                        tx+=1
+                        visit.add(`${tx},${ty}`)
+                    }
+                }
+            }
+
+        }else{
+            // left
+            while(count){
+                hx-=1
+                count--
+                if(hy==ty && hx<tx-1) {
+                    tx-=1
+                    visit.add(`${tx},${ty}`)
+                }else{
+                    if(hx<tx-1 &&hy==ty+1){
+                        ty+=1
+                        tx-=1
+                        visit.add(`${tx},${ty}`)
+                    }else if(hx<tx-1 &&hy==ty-1){
+                        ty-=1
+                        tx-=1
+                        visit.add(`${tx},${ty}`)
+                    }
+                }
+            }
+        }
+    }
+    return visit.size
+}
+
+let testInput=`R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`.trim().split("\n").map(x=>x.split(" "))
+console.log(tailVisited(testInput)==13,"13")
+console.log(tailVisited(input))
+
+// part 2
+
+function getNextHeadPosition(headPos, movement) {
+    const [x, y] = headPos
+    const [direction, amount] = movement
+  
+    switch (direction) {
+      case 'U':
+        return [x, y - amount]
+      case 'D':
+        return [x, y + amount]
+      case 'L':
+        return [x - amount, y]
+      case 'R':
+        return [x + amount, y]
+    }
+  }
+
+
+  function getNextTrailingKnotsPosition(tailPos, headPos) {
+    const [tx, ty] = tailPos
+    const [hx, hy] = headPos
+  
+    const xDiff = hx - tx
+    const yDiff = hy - ty
+  
+    // touching, don't need to move
+    if (Math.abs(xDiff) <= 1 && Math.abs(yDiff) <= 1) return [tx, ty]
+  
+    // 2 left
+    if (hy === ty && tx === hx - 2) {
+      return [tx + 1, ty]
+    }
+  
+    // 2 right
+    if (hy === ty && tx === hx + 2) {
+      return [tx - 1, ty]
+    }
+  
+    // 2 up
+    if (hx === tx && ty === hy - 2) {
+      return [tx, ty + 1]
+    }
+  
+    // 2 down
+    if (hx === tx && ty === hy + 2) {
+      return [tx, ty - 1]
+    }
+  
+    if (hx !== tx && hy !== ty) {
+      const getMethod = num => (num < 0 ? 'floor' : 'ceil')
+  
+      const nextTx = tx + Math[getMethod(xDiff)](xDiff / 2)
+      const nextTy = ty + Math[getMethod(yDiff)](yDiff / 2)
+  
+      return [nextTx, nextTy]
+    }
+  
+  }
+
+function simulateLongRope() {
+    const knotPositions = Array(10).fill([0, 0])
+    let tailLocations = new Set([
+      knotPositions[knotPositions.length - 1].join("-----------"),
+    ])
+  
+    return {
+      getState: () => {
+        return { knotPositions, tailLocations }
+      },
+      tick(movement) {
+        const [direction, amount] = movement
+  
+        for (let i = 0; i < amount; i++) {
+          for (const [index, knot] of knotPositions.entries()) {
+            if (index === 0) {
+              knotPositions[index] = getNextHeadPosition(knot, [direction, 1])
+            } else {
+              knotPositions[index] = getNextTrailingKnotsPosition(
+                knot,
+                knotPositions[index - 1]
+              )
+            }
+  
+            if (index === knotPositions.length - 1) {
+              tailLocations.add(knotPositions[index].join("-----------"))
+            }
+          }
+        }
+      },
+    }
+  }
+  
+  function solution2(input) {
+    
+    const sim = simulateLongRope()
+  
+    for (const move of input) {
+      sim.tick(move)
+    }
+  
+    return sim.getState().tailLocations.size
+  }
+  console.log(solution2(input),"---")
