6ca46fded707aa0e997e979681d358952055bfa0
hsargeant <hssoftwareengineering@gmail.com>
2022-12-28 10:17:13 -0500

    daily coding challenge

---
+++ others/fibo.js
@@ -0,0 +1,18 @@
+// const fibo=(n)=>{
+//     if(n==0) return 0
+//     if (n<2) return 1
+//     return fibo(n-1)+fibo(n-2)
+// }
+
+// console.log(fibo(3))
+
+// loop
+function fiboLoop(n) {
+    let [a, b] = [0, 1];
+    for (let i = 0; i < n; i++) {
+      [a, b] = [b, a+b];
+    }
+    return a;
+  }
+
+console.log(fiboLoop(30))
