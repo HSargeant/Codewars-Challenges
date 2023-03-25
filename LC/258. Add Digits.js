ce75a6a9f8fed49403d79aed4e393ab8ffa955b8
hsargeant <hssoftwareengineering@gmail.com>
2023-03-25 09:58:38 -0400

    daily coding challenge

---
+++ LC/258. Add Digits.js
@@ -0,0 +1,13 @@
+const addDigits = (num)=>{
+  if(num<10) return num
+  return num%9==0 ?9:num%9
+}
+
+// or loop
+// const addDigits = (num)=>{
+//     while(Math.floor(num/10)!==0){
+//       num= [...(num+"")].reduce((a,c)=>a+ +c,0)
+  
+//     }
+//     return num
+//   }
---
+++ LC/453. Minimum Moves to Equal Array Elements.js
@@ -0,0 +1,22 @@
+var minMoves = function(nums) {
+
+    let res=0,min=Math.min(...nums)
+    nums.forEach(x=>{
+        res+=x-min
+    })
+    return res
+};
+
+
+
+// loop/recursion will not work with extreme cases i.e --- [ 1,2,100000]
+// var minMoves = function(nums,count=0) {
+//     if(nums.length==2) return nums.sort((a,b)=>b-a)[0]-nums[1]
+//    if( nums.every(num=>num==nums[0])) return count
+//    let index= nums.indexOf(Math.max(...nums))
+//    nums=nums.map((a,i)=>{
+//        if(i!=index) return a+1
+//        return a
+//    })
+//    return minMoves(nums,count+1)
+// };
