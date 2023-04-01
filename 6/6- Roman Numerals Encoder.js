09ef820bd5ca5c665ecd2f1b815a73c9c082e142
hsargeant <hssoftwareengineering@gmail.com>
2023-04-01 12:17:13 -0500

    daily coding challenge

---
+++ 6/6- Roman Numerals Encoder.js
@@ -0,0 +1,24 @@
+function solution(num){
+  const obj={
+    M:  1000,
+    CM: 900,
+    D:  500,
+    CD: 400,
+    C:  100,
+    XC: 90,
+    L:  50,
+    XL: 40,
+    X:  10,
+    IX: 9,
+    V:  5,
+    IV: 4,
+    I:  1,
+  }
+
+  let res=""
+    for(let x in obj){
+      res+=x.repeat(Math.floor(num/obj[x]))
+      num = num% obj[x]
+    }
+  return res
+}
