4de17e0b28753a6aa4522d8f20cdc43532385ef4
hsargeant <hssoftwareengineering@gmail.com>
2023-08-14 18:01:16 -0400

    daily coding challenge

---
+++ LC/1137. N-th Tribonacci Number,js.js
@@ -0,0 +1,10 @@
+// 1137. N-th Tribonacci Number
+
+var tribonacci = function(n,o={}) {
+    if(n==0) return 0
+    if(n==1 ||n==2) return 1
+    if(o[n]) return o[n]
+    o[n] = tribonacci(n-3,o)+tribonacci(n-2,o) + tribonacci(n-1,o) 
+    return o[n]
+    
+};
