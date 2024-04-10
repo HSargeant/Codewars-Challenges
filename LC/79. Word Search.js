b51cb2db16a72aca25d4805a2a04027a264b810e
hsargeant <hssoftwareengineering@gmail.com>
2024-04-10 14:13:50 -0400

    daily coding challenges

--- LC/79. Word Search.js
+++ LC/79. Word Search.js
@@ -1,18 +1,16 @@
-// 79. Word Search
-
-function dfs(board, r, c, word) {
-    if (word === '') return true;
+function dfs(board, r, c, word,check,i) {
+    if (word === check) return true;
     if (r < 0 || r >= board.length || c < 0 || c >= board[0].length)
         return false;
-    if (board[r][c] !== word[0]) return false;
+    if (board[r][c] !== word[i]) return false;
 
     let temp = board[r][c];
     board[r][c] = '#';
     let result =
-        dfs(board, r - 1, c, word.slice(1)) ||
-        dfs(board, r + 1, c, word.slice(1)) ||
-        dfs(board, r, c - 1, word.slice(1)) ||
-        dfs(board, r, c + 1, word.slice(1));
+        dfs(board, r - 1, c, word,check+temp,i+1) ||
+        dfs(board, r + 1, c, word,check+temp,i+1) ||
+        dfs(board, r, c - 1, word,check+temp,i+1) ||
+        dfs(board, r, c + 1, word,check+temp,i+1);
 
     board[r][c] = temp;
     return result;
@@ -21,10 +19,11 @@ function dfs(board, r, c, word) {
 var exist = function (board, word) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
-            if (dfs(board, i, j, word)) {
+            if (dfs(board, i, j, word,"",0)) {
                 return true;
             }
         }
     }
     return false;
 };
+// 79. Word Search
