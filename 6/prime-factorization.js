86d20163622b1a5099aebbb4eb6181971a9abc7e
hsargeant <hssoftwareengineering@gmail.com>
2024-06-18 22:57:36 -0400

    daily coding challenges

--- 6/prime-factorization.js
+++ 6/prime-factorization.js
@@ -1,18 +1,20 @@
-function PrimeFactorizer(n){
-    let res=[]
+function primeFactorizer(n){
     let obj={}
+    while(n%2==0){
+      obj["2"] =obj["2"] +1 || 1
+      n/=2
+    }
 
-    for(i=2; i<=n; i++){
+    for(i=3; i<=n; i++){
+      // console.log(i,n)
+      let count=0
         while(n%i==0){
-          res.push(i)
+          count++
           n/=i
       }
-    }
-    res.forEach(x=>{
-       obj[x]= obj[x] ? (obj[x]+=1) : 1
-    })
-    
-    return {factor:obj}
+      if(count) obj[i]= count
+    } 
+    return {"factor":obj}
   }
 
 //   The prime factorization of a positive integer is a list of the integer's prime factors, together with their multiplicities; the process of determining these factors is called integer factorization. The fundamental theorem of arithmetic says that every positive integer has a single unique prime factorization.
@@ -22,3 +24,5 @@ function PrimeFactorizer(n){
 // Write a class called PrimeFactorizer whose constructor accepts exactly 1 number (a positive integer) and has a property factor containing an object where the keys are prime numbers as strings and the values are the multiplicities.
 
 // new PrimeFactorizer(24).factor //should return { '2': 3, '3': 1 }
+let x = primeFactorizer(858687)
+console.log(x)
