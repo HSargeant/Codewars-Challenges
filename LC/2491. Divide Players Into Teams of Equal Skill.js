73640d4cba2939bd2f24c1b4bf600086a4fcb171
hsargeant <hssoftwareengineering@gmail.com>
2024-10-04 12:39:08 -0400

    daily coding challenge

---
+++ LC/2491. Divide Players Into Teams of Equal Skill.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} skill
+ * @return {number}
+ */
+function dividePlayers(skill) {
+    skill.sort((a, b) => a - b)
+    let res = 0
+    let sum = skill[0] + skill[skill.length - 1]
+    for (let i = 0; i < skill.length / 2; i++) {
+        if (sum == skill[i] + skill[skill.length - i - 1]) {
+            res += skill[i] * skill[skill.length - i - 1]
+        } else {
+            return -1
+        }
+
+    }
+    return res
+};
+// 2491. Divide Players Into Teams of Equal Skill
