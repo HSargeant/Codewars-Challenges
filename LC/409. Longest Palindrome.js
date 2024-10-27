7ab72074d362110873b55571bbb3033b47a02a26
hsargeant <hssoftwareengineering@gmail.com>
2024-10-27 15:43:41 -0400

    daily coding challenbge

--- LC/409. Longest Palindrome.js
+++ LC/409. Longest Palindrome.js
@@ -1,13 +1,17 @@
-var longestPalindrome = function(s) {
-    let set = new Set()
-    
-        for (let i = 0; i < s.length; i++) {
-            if(set.has(s[i])){
-                set.delete(s[i])
-            }else set.add(s[i])
+function longestPalindrome (s) {
+    let o={},count=0
+    for(let char of s){
+        o[char]=o[char]+1||1
+    }
+    let odd = 0
+    for(let key in o){
+        if(o[key]%2==0) count+=o[key]
+        else{
+            if(o[key]>1) count+=(o[key]-1)
+            odd++
         }
-    
-        if (set.size <= 1) return s.length;
-        return s.length - set.size + 1;
-        
-    };
+    }
+    if(odd>=1) count+=1
+    return count
+};
+// 409. Longest Palindrome
