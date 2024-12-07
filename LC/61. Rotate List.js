c54bf33a3f2ce62d26e258442a08f7ddced1742a
hsargeant <hssoftwareengineering@gmail.com>
2024-12-07 14:19:23 -0500

    daily coding challenge

---
+++ LC/61. Rotate List.js
@@ -0,0 +1,20 @@
+function rotateRight (head, k) {
+    if(!head) return null
+    let a=[]
+    let temp=head
+    while(temp){
+        a.push(temp.val)
+        temp=temp.next
+    }
+    k%=a.length
+    a.unshift(...a.splice(-k))
+    let res= new ListNode(a[0])
+    temp=res
+    let i=0
+    for(let i = 1 ; i<a.length; i++){
+        temp.next = new ListNode(a[i])
+        temp=temp.next
+    }
+    return res
+};
+// 61. Rotate List
